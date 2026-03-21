9171b042fd2df09eb5b9f442d5c254c481514b76
saif-gohar352 <seifgohar352@gmail.com>
2026-03-21 17:56:44 +0200

    for test

---
+++ hi.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=100;
+    cout<<"x is equal "<<x<<endl;
+     //x is equal 2
+     string name="saif gohar";
+     cout<<"my name is "<<name<<endl;
+     double y =2.9;
+     cout<<"y is equal"<<y<<endl;
+     float b =5.3;
+     cout<<"b is equal"<<b<<endl;
+     char v='a';
+     cout<<"v="<<v<<endl;
+     bool success= false;
+     cout<< success<<endl;
+    int age=20;
+    string saif = "ahmed";
+    char gohar ='B';
+   double gpa =3.8;
+    bool ars =true;
+    age =21;
+    cout<<"your name is "<<name<<endl;
+    cout<<"your age is "<<age<<endl;
+     cout<<"your grade is "<<gohar<<endl;
+     cout<<"your gpa is "<<gpa<<endl;
+     cout<<"your success is "<<success<<endl;
+
+    
+
+
+    return 0;
+}
